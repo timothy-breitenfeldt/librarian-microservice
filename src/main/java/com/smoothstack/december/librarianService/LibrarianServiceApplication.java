1aaaf1c9e80415909668f98446a2dce6aab12bb6
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-01-22 00:56:24 -0500

    ...

--- src/main/java/com/smoothstack/december/librarianService/LibrarianServiceApplication.java
+++ src/main/java/com/smoothstack/december/librarianService/LibrarianServiceApplication.java
@@ -1,11 +1,11 @@
 package com.smoothstack.december.librarianService;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@ComponentScan
 public class LibrarianServiceApplication {
 
     public static void main(String[] args) {
