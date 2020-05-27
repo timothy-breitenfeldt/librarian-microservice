f9ed672681ca594938ae6fb1fcab04a548979700
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-05-27 14:03:38 -0400

    Removed component scan anotation from main class.

--- src/main/java/com/smoothstack/lms/librarianservice/LibrarianServiceApplication.java
+++ src/main/java/com/smoothstack/lms/librarianservice/LibrarianServiceApplication.java
@@ -2,13 +2,11 @@ package com.smoothstack.lms.librarianservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import com.smoothstack.lms.librarianservice.swagger.SwaggerConfig;
 
 @SpringBootApplication
-@ComponentScan
 @Import({ SwaggerConfig.class })
 public class LibrarianServiceApplication {
 
